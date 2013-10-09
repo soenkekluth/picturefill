c7f562e02f90cc21d616e9ec45b103d194d84c66
Sönke Kluth <soenke.kluth@gmail.com>
2013-10-09 15:36:46 +0200

    added AMD support call picturefill if document is allready loaded

--- picturefill.js
+++ picturefill.js
@@ -1,62 +1,71 @@
 /*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
 
-(function( w ){
+(function( exports ){
 
-	// Enable strict mode
-	"use strict";
+    // Enable strict mode
+    "use strict";
 
-	w.picturefill = function() {
-		var ps = w.document.getElementsByTagName( "span" );
 
-		// Loop the pictures
-		for( var i = 0, il = ps.length; i < il; i++ ){
-			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
+    var picturefill = function() {
+        var ps = document.getElementsByTagName( "span" );
 
-				var sources = ps[ i ].getElementsByTagName( "span" ),
-					matches = [];
+        // Loop the pictures
+        for( var i = 0, il = ps.length; i < il; i++ ){
+            if( ps[ i ].getAttribute( "data-picture" ) !== null ){
 
-				// See if which sources match
-				for( var j = 0, jl = sources.length; j < jl; j++ ){
-					var media = sources[ j ].getAttribute( "data-media" );
-					// if there's no media specified, OR w.matchMedia is supported 
-					if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
-						matches.push( sources[ j ] );
-					}
-				}
+                var sources = ps[ i ].getElementsByTagName( "span" ),
+                    matches = [];
 
-			// Find any existing img element in the picture element
-			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+                // See if which sources match
+                for( var j = 0, jl = sources.length; j < jl; j++ ){
+                    var media = sources[ j ].getAttribute( "data-media" );
+                    // if there's no media specified, OR window.matchMedia is supported
+                    if( !media || ( window.matchMedia && window.matchMedia( media ).matches ) ){
+                        matches.push( sources[ j ] );
+                    }
+                }
 
-			if( matches.length ){
-				var matchedEl = matches.pop();
-				if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
-					picImg = w.document.createElement( "img" );
-					picImg.alt = ps[ i ].getAttribute( "data-alt" );
-				}
-				if(picImg.src !== matchedEl.getAttribute( "data-src" )){
-					picImg.src =  matchedEl.getAttribute( "data-src" );
-				}
-				matchedEl.appendChild( picImg );
-			}
-			else if( picImg ){
-				picImg.parentNode.removeChild( picImg );
-			}
-		}
-		}
-	};
+            // Find any existing img element in the picture element
+            var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
-	// Run on resize and domready (w.load as a fallback)
-	if( w.addEventListener ){
-		w.addEventListener( "resize", w.picturefill, false );
-		w.addEventListener( "DOMContentLoaded", function(){
-			w.picturefill();
-			// Run once only
-			w.removeEventListener( "load", w.picturefill, false );
-		}, false );
-		w.addEventListener( "load", w.picturefill, false );
-	}
-	else if( w.attachEvent ){
-		w.attachEvent( "onload", w.picturefill );
-	}
+            if( matches.length ){
+                var matchedEl = matches.pop();
+                if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
+                    picImg = document.createElement( "img" );
+                    picImg.alt = ps[ i ].getAttribute( "data-alt" );
+                }
+                if(picImg.src !== matchedEl.getAttribute( "data-src" )){
+                    picImg.src =  matchedEl.getAttribute( "data-src" );
+                }
+                matchedEl.appendChild( picImg );
+            }
+            else if( picImg ){
+                picImg.parentNode.removeChild( picImg );
+            }
+        }
+        }
+    };
 
-}( this ));
+    // Run on resize and domready (w.load as a fallback)
+    if( window.addEventListener ){
+        window.addEventListener( "resize", picturefill, false );
+        window.addEventListener( "DOMContentLoaded", function(){
+            picturefill();
+            // Run once only
+            window.removeEventListener( "load", picturefill, false );
+        }, false );
+        window.addEventListener( "load", picturefill, false );
+    }
+    else if( window.attachEvent ){
+        window.attachEvent( "onload", picturefill );
+    }
+
+    if (document.readyState == "complete" || document.readyState == "loaded") {
+        picturefill();
+    }
+
+(typeof module != "undefined" && module.exports) ? (module.exports = picturefill) : (typeof define != "undefined" ? (define(function () {
+        return picturefill;
+    })) : (exports.picturefill = picturefill));
+
+})(this);
