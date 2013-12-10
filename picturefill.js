4e2fef11dd572cf17288416407355d287826bd61
Sönke Kluth <soenke.kluth@gmail.com>
2013-12-10 15:42:01 +0100

    added complete listener to images and let the picture element fire a complete event when the image is loaded

--- picturefill.js
+++ picturefill.js
@@ -6,51 +6,65 @@
     "use strict";
 
 
+    var initCompleteListener = function(picImg, picElem){
+        var onComplete = function(event) {
+            event.target.removeEventListener("load", onComplete);
+            try{
+                var evt = document.createEvent("Event");
+                evt.initEvent("load", true, true);
+                picElem.dispatchEvent(evt);
+            }catch(e){
+                console.log("Error:", e);
+            }
+        };
+
+        picImg.addEventListener("load", onComplete, false);
+    }
+
+
     var picturefill = function() {
-        var ps = document.getElementsByTagName("span"),
-            mediaList,
-            sources,
-            matches,
-            hasMatch,
-            picImg,
-            media;
+
+        var ps = document.getElementsByTagName("span");
 
         // Loop the pictures
         for (var i = 0, il = ps.length; i < il; i++) {
             if (ps[i].getAttribute("data-picture") !== null) {
 
-                sources = ps[i].getElementsByTagName("span"),
+                var sources = ps[i].getElementsByTagName("span"),
                 matches = [];
 
                 // See if which sources match
-                for( var j = 0, jl = sources.length; j < jl; j++ ){
-                    var media = sources[ j ].getAttribute( "data-media" );
-                    // if there's no media specified, OR w.matchMedia is supported
-                    if( !media || ( window.matchMedia && window.matchMedia( media ).matches ) ){
-                        matches.push( sources[ j ] );
+                for (var j = 0, jl = sources.length; j < jl; j++) {
+                    var media = sources[j].getAttribute("data-media");
+                    // if there's no media specified, OR window.matchMedia is supported
+                    if (!media || (window.matchMedia && window.matchMedia(media).matches)) {
+                        matches.push(sources[j]);
                     }
                 }
 
-                //console.log(matches.concat());
-
                 // Find any existing img element in the picture element
-                picImg = ps[i].getElementsByTagName("img")[0];
+                var picImg = ps[i].getElementsByTagName("img")[0];
 
                 if (matches.length) {
-
                     var matchedEl = matches.pop();
-                    //console.log(matchedEl);
-
                     if (!picImg || picImg.parentNode.nodeName === "NOSCRIPT") {
                         picImg = document.createElement("img");
-                        if(ps[i].getAttribute("data-alt")){
-                            picImg.alt = ps[i].getAttribute("data-alt");
-                        }
+                        picImg.alt = ps[i].getAttribute("data-alt");
+                    } else if (matchedEl === picImg.parentNode) {
+                        // Skip further actions if the correct image is already in place
+                        continue;
                     }
-                    if (picImg.src !== matchedEl.getAttribute("data-src")) {
-                        picImg.src = matchedEl.getAttribute("data-src");
-                    }
+
+
+
+                    initCompleteListener(picImg,ps[i] );
+
+                    picImg.removeAttribute("width");
+                    picImg.removeAttribute("height");
+                    picImg.src = matchedEl.getAttribute("data-src");
+
                     matchedEl.appendChild(picImg);
+
                 } else if (picImg) {
                     picImg.parentNode.removeChild(picImg);
                 }
@@ -61,12 +75,12 @@
     // Run on resize and domready (w.load as a fallback)
     if (window.addEventListener) {
         window.addEventListener("resize", picturefill, false);
-        window.addEventListener("DOMContentLoaded", function() {
-            picturefill();
-            // Run once only
-            window.removeEventListener("load", picturefill, false);
-        }, false);
-        window.addEventListener("load", picturefill, false);
+        // window.addEventListener("DOMContentLoaded", function() {
+        //     picturefill();
+        //     // Run once only
+        //     window.removeEventListener("load", picturefill, false);
+        // }, false);
+        // window.addEventListener("load", picturefill, false);
     } else if (window.attachEvent) {
         window.attachEvent("onload", picturefill);
     }
